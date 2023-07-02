61a87407db521fd1e4eb7fb709fb0a7bfdb7d78b
Rifang <rifang.asmara12@gmail.com>
2023-07-02 12:57:16 +0800

    Add kode akses

--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,12 @@ import styles from "./page.module.css";
 export default function Home() {
   const [form, setForm] = useState("");
   const [number, setNumber] = useState(0);
+  const [auth, setAuth] = useState(false);
+  const [kode, setKode] = useState("");
+  const [error, setError] = useState(false);
   const asset = [
     {
-      question: "dampak yang dapat ditimbulkan oleh penyebaran viru ebola secara luas terhadap masyarakt dunia adalah ...",
+      question: "dampak yang dapat ditimbulkan oleh penyebaran virus ebola secara luas terhadap masyarakt dunia adalah ...",
       answer: [
         ["membatasi ruang gerak dan interaksi masyarakat", true],
         ["menimbulkan rasa takur melakukan untuk perjalanan jauh", false],
@@ -36,6 +39,20 @@ export default function Home() {
     },
   ];
   const [data, setData] = useState(asset);
+  const handleChangeForm = (e) => {
+    e.preventDefault();
+    const { value } = e.target;
+    setKode(value);
+  };
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    if (kode === "iloveyou") {
+      setError(false);
+      setAuth(true);
+    } else {
+      setError(true);
+    }
+  };
   const handleChange = (e) => {
     setData(asset);
     e.preventDefault();
@@ -58,36 +75,56 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.leftContainer}>
-          <Image src="/anime.png" alt="logo" width={240} height={150} style={{ position: "relative", zIndex: 10 }} />
-          <Image src="/ellipse.png" alt="logo" width={330} height={330} style={{ position: "absolute" }} />
-        </div>
-        <div className={styles.rightContainer}>
-          <h1 className={styles.headerTitle}>Masukan keywords</h1>
-          <div className={styles.topContainer}>
-            <input className={styles.keywordForm} id="keyword" name="keyword" type="text" onChange={handleChange} />
+      {!auth ? (
+        <div className={styles.authcontainer}>
+          <div className={styles.authleftContainer}>
+            <Image src="/anime.png" alt="logo" width={440} height={300} style={{ position: "relative", zIndex: 10 }} />
+            <Image src="/ellipse.png" alt="logo" width={630} height={630} style={{ position: "absolute" }} />
           </div>
-          <div className={styles.bottomContainer}>
-            {data.map((item, index) => (
-              <div className={styles.card} key={index}>
-                <div className={styles.qustionContainer}>
-                  <span className={styles.questionChoice}>{item.question.slice(0, number)}</span>
-                  <span className={styles.question}>{item.question.slice(number)}</span>
-                </div>
-                <div className={styles.answerContainer}>
-                  {item.answer.map((item, index) => (
-                    <p key={index} className={item[1] ? styles.answerTrue : styles.answerFalse}>
-                      {(index === 0 ? "A. " : index === 1 ? "B. " : index === 2 ? "C. " : "D. ") + item[0]}
-                    </p>
-                  ))}
-                </div>
-                <p className={styles.answerFinal}>{item.answer[0][1] ? "Jawaban : A" : item.answer[1][1] ? "Jawaban : B " : item.answer[2][1] ? "Jawaban : C " : "Jawaban : D"}</p>
-              </div>
-            ))}
+          <div className={styles.authrightContainer}>
+            <div className={styles.formContainer}>
+              <h1 style={{ fontSize: 60, fontWeight: 800, marginBottom: 30 }}>Kode Akses</h1>
+              <input onChange={handleChangeForm} value={kode} name="kode" type="password" className={styles.formStyle} placeholder="Masukan Kose Akses" />
+              {error ? <p style={{ fontSize: 10, color: "red" }}>* Kode akses salah, silahkan coba lagi</p> : null}
+              <button onClick={handleSubmitForm} className={styles.buttonForm}>
+                Masuk
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className={styles.container}>
+          <div className={styles.leftContainer}>
+            <Image src="/anime.png" alt="logo" width={240} height={150} style={{ position: "relative", zIndex: 10 }} />
+            <Image src="/ellipse.png" alt="logo" width={330} height={330} style={{ position: "absolute" }} />
+          </div>
+          <div className={styles.rightContainer}>
+            <h1 className={styles.headerTitle}>Masukan keywords</h1>
+            <div className={styles.topContainer}>
+              <input className={styles.keywordForm} id="keyword" name="keyword" type="text" onChange={handleChange} />
+            </div>
+            <div className={styles.bottomContainer}>
+              {data.map((item, index) => (
+                <div className={styles.card} key={index}>
+                  <div className={styles.qustionContainer}>
+                    <span className={styles.question}>{item.question}</span>
+                    {/* <span className={styles.questionChoice}>{item.question.slice(0, number)}</span>
+                    <span className={styles.question}>{item.question.slice(number)}</span> */}
+                  </div>
+                  <div className={styles.answerContainer}>
+                    {item.answer.map((item, index) => (
+                      <p key={index} className={item[1] ? styles.answerTrue : styles.answerFalse}>
+                        {(index === 0 ? "A. " : index === 1 ? "B. " : index === 2 ? "C. " : "D. ") + item[0]}
+                      </p>
+                    ))}
+                  </div>
+                  <p className={styles.answerFinal}>{item.answer[0][1] ? "Jawaban : A" : item.answer[1][1] ? "Jawaban : B " : item.answer[2][1] ? "Jawaban : C " : "Jawaban : D"}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
     </main>
   );
 }
