7a0bb50153a9b265d19c5a9f181bf7688fe3e625
Rifang <rifang.asmara12@gmail.com>
2023-07-05 23:16:53 +0800

    change name

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,17 @@
-import { Inter } from 'next/font/google';
-import './globals.css';
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Anggy App',
-  description: 'Find the answer by Anggy app',
-}
+  title: "My Answer App",
+  description: "Find the answer by Anggy app",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
